578094f07e2640a484cebfe459e771022889f5e8
Abed Halawi <halawi.abed@gmail.com>
2013-06-02 04:11:48 +0300

    Initial Commit - includes config file

---
+++ src/config/flickr.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+	'api_key'           => '',
+	'secret_key'        => '',
+
+	'feed_host_pattern' => 'http://api.flickr.com?%s=%s&format=xml',
+	'api_host'          => 'http://api.flickr.com/services/rest/',
+	'sets_method'       => 'flickr.photosets.getPhotos'
+
+);
