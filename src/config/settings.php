9c0aff7719431223072816c69ed1c402e54cac18
Abed Halawi <halawi.abed@gmail.com>
2013-06-03 19:45:52 +0300

    Update settings keys

--- src/config/settings.php
+++ src/config/settings.php
@@ -2,11 +2,11 @@
 
 return array(
 
-	'api_key'           => '49038bbbfd63f9f5ae2363ad6485c30c',
-	'secret_key'        => '1aef4c4213394275',
+	'api_key'                => '49038bbbfd63f9f5ae2363ad6485c30c',
+	'secret_key'             => '1aef4c4213394275',
 
-	'feed_host_pattern' => 'http://api.flickr.com%s?%s=%s&format=php_serial',
-	'api_host'          => 'http://api.flickr.com/services/rest/',
-	'sets_method'       => 'flickr.photosets.getPhotos'
+	'feed_host_pattern'      => 'http://api.flickr.com%s?%s=%s&format=php_serial',
+	'api_host'               => 'http://api.flickr.com/services/rest/',
+	'photoset_photos_method' => 'flickr.photosets.getPhotos'
 
 );
