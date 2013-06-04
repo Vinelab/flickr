e0c0f1b10db47c807bf5a40ca42edc0eb4ba4249
Abed Halawi <halawi.abed@gmail.com>
2013-06-04 15:13:08 +0300

    Remove config settings

--- src/config/settings.php
+++ src/config/settings.php
@@ -2,8 +2,8 @@
 
 return array(
 
-	'api_key'                => '49038bbbfd63f9f5ae2363ad6485c30c',
-	'secret_key'             => '1aef4c4213394275',
+	'api_key'                => '',
+	'secret_key'             => '',
 
 	'feed_host_pattern'      => 'http://api.flickr.com%s?%s=%s&format=php_serial',
 	'api_host'               => 'http://api.flickr.com/services/rest/',
